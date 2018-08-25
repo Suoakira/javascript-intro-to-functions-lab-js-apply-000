b071770ebb8a626444ffbf0804151ef01b79508b
Stephen James <steves0089@gmail.com>
2018-08-25 16:10:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 
 
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase(); // .toUpperCase() makes strings uppercase
 }
+
+function whisper(string) {
+  return string.toLowerCase(); // toLowerCase() makes strings to lower case
+}
+
+function logShout(string) {
+  console.log(string.toUppercase())
+}
+
