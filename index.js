9ffff12ead95595cd81587d5ae62bd3ac6d32d56
Stephen James <steves0089@gmail.com>
2018-08-25 16:15:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I cant't hear you!"
-  } else if 
-    (string === string.toUpperCase()) {
-      return "YES INDEED!"
-    }
+    return "I cant't hear you!";
+  } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+  } else if ( string === "I love you, Grandma") {
+    return "I love you, too";
   }
 }
 
